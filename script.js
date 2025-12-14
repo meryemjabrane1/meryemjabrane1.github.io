a4c0ab0fad1be52ddcdc5d5fcf5bf220d04ff2ec
Meryem Jabrane <M.Jabrane@aui.ma>
2025-12-14 14:36:17 +0000

    color change

--- script.js
+++
@@ -1,6 +0,0 @@
-// Example: change header color on scroll
-window.addEventListener('scroll', () => {
-    const header = document.querySelector('header');
-    header.style.background = window.scrollY > 50 ? '#3730a3' : '#4f46e5';
-  });
-  
